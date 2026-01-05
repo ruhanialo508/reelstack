b7ba5ba40d028597faa7e7ef5edb907174e2b2cc
ruhanialo508 <ruhani.alo508@gmail.com>
2026-01-05 06:00:25 +0530

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,17 @@
+import "./globals.css";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#000000" />
+      </head>
+      <body>{children}</body>
+    </html>
+  );
+}
